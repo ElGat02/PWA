fe1ec198d36d1f23f3651e9ce2b0561dc766a888
ElGato02 <crepy27@gmail.com>
2023-11-23 08:56:51 -0500

    agregacion de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"tarea-pwa-64913","appId":"1:190842821617:web:76a23b1a04c6842e6d4250","storageBucket":"tarea-pwa-64913.appspot.com","apiKey":"AIzaSyDw139SMT11JaRUJOuLcdIq02pfIPclZTE","authDomain":"tarea-pwa-64913.firebaseapp.com","messagingSenderId":"190842821617"})),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
